19a3d52b19abd5c5bb0fb345e528a8da41399979
espi3000 <60798059+espi3000@users.noreply.github.com>
2022-07-11 09:33:49 +0200

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,44 +1,44 @@
 #include <WiFi.h>
-#include <ESPAsyncWebServer.h>
+#include <WebServer.h>
+#include <WebSocketServer.h>
 #include <SPIFFS.h>
-#include <AsyncTCP.h>
-#include <Wire.h>
-#include <string.h>
 #include <Adafruit_ADS1015.h>
 
 Adafruit_ADS1115 adc;
-AsyncWebServer server(80); // Create AsyncWebServer object on port 80
+WebServer http(80);             // Create HTTP server object on port 80
+WebSocketsServer webSocket(81); // Create WebSockets server object on port 81
 
-const char* SSID = "Holsen 2.4";
-const char* PASS = "2311021058";
+const char* SSID = "";
+const char* PASS = "";
 
-void ADC_init() {              //Initialization for the ADC, gain is set to one, giving a range of +-6V for differential measurements
-    adc.setGain(GAIN_ONE);
-    adc.begin();
- }
+void handleGetRequest() {
+    File webpage = SPIFFS.open("/index.html", "r"); // Open file located in data directory
+    http.streamFile(webpage, "text/html");          // Return webpage with 200 OK response
+    webpage.close();                                // Close the file to clear resources 
+}
 
-String ecgADC() {             //ECG measuring function to measure the value of the analog output. Converts data, typecasts to string and returns string
-    float t = adc.readADC_Differential_0_1(); // Read ECG values (the default)
-    t = waveform();
-
-    if (isnan(t)) {    
-        Serial.println("Failed to read from ECG board!");
-        return "";
-    } else {
-        Serial.println(t);
-        return String(t);
+void broadcastWebSocket(int16_t) {
+    if (!adc.conversionComplete()) {
+        return; // Returns if no data is available
     }
+    int16_t reading = adc.getLastConversionResults();
+    adc.startADCReading(ADS1X15_REG_CONFIG_MUX_DIFF_0_1, false);
+    // float volts = adc.computeVolts(reading)
+    
+    char json[14]; // int16_t is max 6 bytes when formated as char*
+    sprintf(json, "{\"ECG\":%d}", reading);
+    webSocket.broadcastTXT(json);
 }
 
 void setup() {
-    // Serial port for debugging purposes
-    Serial.begin(115200);
-    ADC_init();
+    Serial.begin(115200); // Serial port for debugging purposes
+
+    adc.setGain(GAIN_ONE); // TWOTHIRDS Remember multiplier
+    adc.begin();
+    // adc.startADCReading(ADS1X15_REG_CONFIG_MUX_DIFF_0_1, false); // Start first reading
     
-    // Initialize SPIFFS
-    if (!SPIFFS.begin()) {
+    if (!SPIFFS.begin()) { // Initialize SPIFFS
         Serial.println("An Error has occurred while mounting SPIFFS");
-        return;
     }
     
     WiFi.begin(SSID, PASS); // Connect to Wi-Fi
@@ -46,25 +46,26 @@ void setup() {
         delay(1000);
         Serial.println("Connecting to WiFi..");
     }
-    
     Serial.println(WiFi.localIP());  // Print ESP32 Local IP Address
     
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){ // Route for root / web page
-        request->send(SPIFFS, "/index.html");
-    });
-    server.on("/ECG", HTTP_GET, [](AsyncWebServerRequest *request){
-        request->send_P(200, "text/plain", ecgADC().c_str());
-    });
-
-    server.begin(); // Start server
+    http.on("/", handleGetRequest); // The pointer to our function is passed
+    http.begin();
+    webSocket.begin();
 }
  
-void loop() {}
+void loop() {
+    http.handleClient();
+    webSocket.loop();
 
+    int16_t reading = adc.readADC_Differential_0_1(); // This line of code is blocking from execution until a conversion is done (worst case 1.16ms for 860S/s)
+    char* readingTxt = itoa(reading); // Convert int16_t to char*
+    webSocket.broadcastTXT(readingTxt); // This function cannot use ints larger than int8_t
+}
 
-int16_t pulseTrain(uint16_t period) {
-    static int16_t time = millis();
-    static int16_t state = false;
+/*
+uint16_t pulseTrain(uint16_t period) {
+    static uint16_t time = millis();
+    static uint16_t state = false;
     if ((millis() - time) > period/2) {
         time = millis();
         state = !state;
@@ -72,6 +73,7 @@ int16_t pulseTrain(uint16_t period) {
     return state;
 }
 
-int16_t waveform() {
-    return 100*pulseTrain(10000) + random(10) - 50;
+uint16_t waveform() {
+    return 100*pulseTrain(1000) + random(50) - 25;
 }
+*/
